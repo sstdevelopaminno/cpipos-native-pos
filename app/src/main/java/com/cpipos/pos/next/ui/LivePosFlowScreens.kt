b79c32c715abf1adf33b22b2658b18135d8ad6d5
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-25 23:57:15 +0700

    fix(android): import Compose background for cashier device status cards

--- app/src/main/java/com/cpipos/pos/next/ui/LivePosFlowScreens.kt
+++ app/src/main/java/com/cpipos/pos/next/ui/LivePosFlowScreens.kt
@@ -1,6 +1,7 @@
 package com.cpipos.pos.next.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
